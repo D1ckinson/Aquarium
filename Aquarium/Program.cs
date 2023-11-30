52720c208d62d4369fc3bc778ca58fc073426165
Макс Крячков <123maxik321@mail.ru>
2023-11-30 20:25:34 +0300

    1.0

--- Aquarium/Program.cs
+++ Aquarium/Program.cs
@@ -12,7 +12,7 @@ namespace Aquarium
 
             Aquarium aquarium = new Aquarium();
             ActionBuilder actionBuilder = new ActionBuilder(aquarium);
-            Menu menu = new Menu(actionBuilder.GiveActions());
+            Menu menu = new Menu(actionBuilder.GiveActions(), actionBuilder.GiveDrawAquariumAction());
 
             menu.Work();
         }
@@ -20,17 +20,24 @@ namespace Aquarium
 
     class Fish
     {
-        private int _age;
+        private int _age = 0;
         private int _maxAge = 10;
 
-        public Fish() =>
-            _age = 0;
+        private string _deadMessage = "Я мертвая рыбка.";
 
-        public bool IsAlive =>
-            _age < _maxAge;
+        public bool IsAlive => _age < _maxAge;
+        private string AliveMessage => $"Я рыбка, мой возраст - {_age}.";
 
-        public void GetOlder() =>
+        public void Grow()
+        {
+            if (IsAlive == false)
+                return;
+
             _age++;
+        }
+
+        public string TellStatus() =>
+            IsAlive ? AliveMessage : _deadMessage;
     }
 
     class Aquarium
@@ -38,38 +45,72 @@ namespace Aquarium
         private List<Fish> _fish = new List<Fish>();
         private int _capacity = 5;
 
-        public Aquarium() { }
+        private bool IsFull =>
+            _capacity <= _fish.Count;
 
-        public void AddFish(Fish fish) =>
-            _fish.Add(fish);
+        public string[] TellFishStatus()
+        {
+            string[] fishStatus = new string[_capacity];
+
+            for (int i = 0; i < _fish.Count; i++)
+                fishStatus[i] = _fish[i].TellStatus();
+
+            return fishStatus;
+        }
+
+        public void AddFish(Fish fish)
+        {
+            if (IsFull == false)
+                _fish.Add(fish);
+        }
 
         public void RemoveDeadFish() =>
             _fish.RemoveAll(fish => fish.IsAlive == false);
 
-        public void LiveYear() =>
-            _fish.ForEach(fish => fish.GetOlder());
+        public void Live() =>
+            _fish.ForEach(fish => fish.Grow());
     }
 
     class Menu
     {
-        const ConsoleKey MoveSelectionUp = ConsoleKey.UpArrow;
-        const ConsoleKey MoveSelectionDown = ConsoleKey.DownArrow;
-        const ConsoleKey ConfirmSelection = ConsoleKey.Enter;
-
-        private ConsoleColor _backgroundColor = ConsoleColor.White;//название
-        private ConsoleColor _foregroundColor = ConsoleColor.Black;//название
+        private const ConsoleKey MoveSelectionUp = ConsoleKey.UpArrow;
+        private const ConsoleKey MoveSelectionDown = ConsoleKey.DownArrow;
+        private const ConsoleKey ConfirmSelection = ConsoleKey.Enter;
 
         private int _itemIndex = 0;
         private bool _isRunning;
         private string[] _items;
 
         private Dictionary<string, Action> _actions = new Dictionary<string, Action>();
+        private Action _drawAquarium;
 
-        public Menu(Dictionary<string, Action> actions)
+        public Menu(Dictionary<string, Action> actions, Action drawAquarium)
         {
             _actions = actions;
             _actions.Add("Выход", Exit);
             _items = _actions.Keys.ToArray();
+            _drawAquarium = drawAquarium;
+        }
+
+        private int ItemIndex
+        {
+            get
+            {
+                return _itemIndex;
+            }
+
+            set
+            {
+                int lastIndex = _items.Length - 1;
+
+                if (value > lastIndex)
+                    value = lastIndex;
+
+                if (value < 0)
+                    value = 0;
+
+                _itemIndex = value;
+            }
         }
 
         public void Work()
@@ -78,45 +119,24 @@ namespace Aquarium
 
             while (_isRunning)
             {
-                DrawItems();
+                Renderer.DrawMenu(_items, _itemIndex);
+
+                _drawAquarium.Invoke();
 
                 ReadKey();
-
-                ClampIndex();
             }
-        }
-
-        private void DrawItems()
-        {
-            Console.SetCursorPosition(0, 0);
-
-            for (int i = 0; i < _items.Length; i++)
-                if (i == _itemIndex)
-                    WriteColoredText(_items[i]);
-                else
-                    Console.WriteLine(_items[i]);
-        }
-
-        private void WriteColoredText(string text)
-        {
-            Console.ForegroundColor = _foregroundColor;
-            Console.BackgroundColor = _backgroundColor;
-
-            Console.WriteLine(text);
-
-            Console.ResetColor();
         }
 
         private void ReadKey()
         {
-            switch (Console.ReadKey().Key)
+            switch (Console.ReadKey(true).Key)
             {
                 case MoveSelectionDown:
-                    _itemIndex++;
+                    ItemIndex++;
                     break;
 
                 case MoveSelectionUp:
-                    _itemIndex--;
+                    ItemIndex--;
                     break;
 
                 case ConfirmSelection:
@@ -125,17 +145,8 @@ namespace Aquarium
             }
         }
 
-        private void ClampIndex()
-        {
-            int lastIndex = _items.Length - 1;
-
-            if (_itemIndex > lastIndex)
-                _itemIndex = lastIndex;
-            else if (_itemIndex < 0)
-                _itemIndex = 0;
-        }
-
-        private void Exit() => _isRunning = false;
+        private void Exit() =>
+            _isRunning = false;
     }
 
     class ActionBuilder
@@ -157,6 +168,9 @@ namespace Aquarium
             return actions;
         }
 
+        public Action GiveDrawAquariumAction() =>
+            () => Renderer.DrawAquarium(_aquarium.TellFishStatus());
+
         private void BuyFish() =>
             _aquarium.AddFish(new Fish());
 
@@ -164,17 +178,57 @@ namespace Aquarium
             _aquarium.RemoveDeadFish();
 
         private void LiveYear() =>
-            _aquarium.LiveYear();
+            _aquarium.Live();
     }
 
-    static class UserUtilities//название
+    class Renderer
     {
-        private static Random s_random = new Random();
+        private static ConsoleColor _backgroundColor = ConsoleColor.White;
+        private static ConsoleColor _foregroundColor = ConsoleColor.Black;
 
-        public static int Next(int value) => s_random.Next(value);
+        private static int aquariumCursorPositionY = 10;
+        private static int _lineSize = 30;
 
-        public static int Next(int minValue, int maxValue) => s_random.Next(minValue, maxValue);
+        private static char _aquariumBorder = '-';
+        private static char _spaceChar = ' ';
 
+        public static void DrawMenu(string[] items, int index)
+        {
+            Console.SetCursorPosition(0, 0);
 
+            for (int i = 0; i < items.Length; i++)
+                if (i == index)
+                    WriteColoredText(items[i]);
+                else
+                    Console.WriteLine(items[i]);
+        }
+
+        public static void DrawAquarium(string[] infoArray)
+        {
+            Console.CursorTop = aquariumCursorPositionY;
+
+            Console.WriteLine(new string(_aquariumBorder, _lineSize));
+
+            foreach (string info in infoArray)
+            {
+                Console.Write(new string(_spaceChar, _lineSize));
+
+                Console.CursorLeft = 0;
+
+                Console.WriteLine(info);
+            }
+
+            Console.WriteLine(new string(_aquariumBorder, _lineSize));
+        }
+
+        private static void WriteColoredText(string text)
+        {
+            Console.ForegroundColor = _foregroundColor;
+            Console.BackgroundColor = _backgroundColor;
+
+            Console.WriteLine(text);
+
+            Console.ResetColor();
+        }
     }
 }
